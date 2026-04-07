e84b23eee34bee9aa8a67ab24bc98eb908896c29
André Leite <leite@de.ufpe.br>
2026-04-07 16:59:34 -0300

    update

--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,3 @@
-#' Check if a package is installed and load it
-#'
-#' @description
-#' `r lifecycle::badge("deprecated")`
-#'
-#' This function is deprecated because all required packages are declared in
-#' the package DESCRIPTION file and are automatically available. There is no
-#' need to check or load them at runtime.
-#'
-#' @param package_name A string with the name of the package to check and load.
-#'
-#' @return Invisibly returns `TRUE` if the package namespace is available.
-#' @import utils
-#'
-#' @examples
-#' check_and_load_package("httr2")
-#' @export
-check_and_load_package <- function(package_name) {
-  cli::cli_warn(c(
-    "{.fn check_and_load_package} is deprecated.",
-    "i" = "All required packages are declared in DESCRIPTION and loaded automatically."
-  ))
-  invisible(requireNamespace(package_name, quietly = TRUE))
-}
-
 #' Handle HTTP response from Server
 #'
 #' This function processes the response from the WebDAV server, checking for errors.
--- docs/articles/index.md
+++
@@ -1,8 +0,0 @@
-# Articles
-
-### All vignettes
-
-- [Using Environment Variables for Security in the webdav
-  Package](https://monitoramento.sepe.pe.gov.br/webdav/articles/env_variables.md):
-- [Get
-  Started](https://monitoramento.sepe.pe.gov.br/webdav/articles/webdav.md):
